9a9dee45bdb86eda30e8b2001f79e98920538f73
Alex Ov <alexov@swisscows.email>
2024-04-08 16:35:57 +0300

    Change folder name creation to new better approach (two substring between '/' after 'github.com'

--- gcp.py
+++ gcp.py
@@ -8,9 +8,21 @@ def check_sys_argv():
         exit(1)
 
 
-def compose_command_line_string(arg_str: str) -> str:
-    str_parts = arg_str.split('/')
-    command_line_str = ' '.join(['git clone', arg_str, str_parts[-2] + '_' + str_parts[-1]])
+def folder_string(repo_url_str: str) -> str:
+    # remove trailing '.git' substring
+    pos = repo_url_str.rfind('.git')
+    if pos > 0:
+        repo_url_str = repo_url_str[:-4]
+
+    # split and compose folder string from what is between '/' after 'github.com'
+    str_parts = repo_url_str.split('/')
+    github_index = str_parts.index("github.com")
+    return_string = str_parts[github_index + 1] + '_' + str_parts[github_index + 2]
+    return return_string
+
+
+def compose_command_line_string(repo_url_str: str) -> str:
+    command_line_str = ' '.join(['git clone', repo_url_str, folder_string(repo_url_str)])
     return command_line_str
 
 
