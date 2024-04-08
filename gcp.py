5da576d1a2745516af264a15c0459a28a6f990ed
Alex Ov <alexov@swisscows.email>
2024-04-08 17:01:31 +0300

    Add code and tests for separate creation of filder url

--- gcp.py
+++ gcp.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from typing import List
 
 
 def check_sys_argv():
@@ -8,21 +9,31 @@ def check_sys_argv():
         exit(1)
 
 
-def folder_string(repo_url_str: str) -> str:
+def prepare_and_split_string(repo_url_str: str) -> List[str]:
     # remove trailing '.git' substring
     pos = repo_url_str.rfind('.git')
     if pos > 0:
         repo_url_str = repo_url_str[:-4]
 
     # split and compose folder string from what is between '/' after 'github.com'
-    str_parts = repo_url_str.split('/')
+    return repo_url_str.split('/')
+
+
+def folder_string(repo_url_str: str) -> str:
+    str_parts = prepare_and_split_string(repo_url_str)
     github_index = str_parts.index("github.com")
     return_string = str_parts[github_index + 1] + '_' + str_parts[github_index + 2]
     return return_string
 
 
+def repo_string(repo_url_str: str) -> str:
+    str_parts = prepare_and_split_string(repo_url_str)
+    slice_str_parts = str_parts[0:5]
+    return '/'.join(slice_str_parts)
+
+
 def compose_command_line_string(repo_url_str: str) -> str:
-    command_line_str = ' '.join(['git clone', repo_url_str, folder_string(repo_url_str)])
+    command_line_str = ' '.join(['git clone', "", folder_string(repo_url_str)])
     return command_line_str
 
 
--- test_gcp.py
+++ test_gcp.py
@@ -14,3 +14,18 @@ def test_folder_name_repository_url_plus_git():
 def test_folder_name_file_url():
     assert "mr-alexov_aov_automation_docs" == folder_string(
         "https://github.com/mr-alexov/aov_automation_docs/blob/master/src/test/java/examples/FightThePopups.java")
+
+
+def test_repo_url_clean_repository_url():
+    assert "https://github.com/mr-alexov/aov_automation_docs" == repo_string(
+        "https://github.com/mr-alexov/aov_automation_docs")
+
+
+def test_repo_url_repository_url_plus_git():
+    assert "https://github.com/mr-alexov/aov_automation_docs" == repo_string(
+        "https://github.com/mr-alexov/aov_automation_docs.git")
+
+
+def test_repo_url_file_url():
+    assert "https://github.com/mr-alexov/aov_automation_docs" == repo_string(
+        "https://github.com/mr-alexov/aov_automation_docs/blob/master/src/test/java/examples/FightThePopups.java")
